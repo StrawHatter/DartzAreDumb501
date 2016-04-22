8130eae5d66de639a9eac7e88e13c7c42b8a7e0c
Paul Dreczkowski <StrawHatter@users.noreply.github.com>
2016-04-22 07:34:34 +0100

    Update Darts501.h

--- Darts501.h
+++ Darts501.h
@@ -1,3 +1,11 @@
+//
+//  Darts501.h
+//  Darts501
+//
+//  Created by Cordelia on 22/04/2016.
+//  Copyright © 2016 Cordelia. All rights reserved.
+//
+
 #ifndef _DARTS_501_CLASS_H
 #define _DARTS_501_CLASS_H
 
@@ -6,18 +14,18 @@ extern const int N;
 
 class Darts_501{
 public:
-
-	int accuracy;
-	int score;
-	state curr_state;
-	int darts;
-	int bull(int p);
-	int throw_single(int single);
-	int throw_double(int d);
-	int throw_treble(int d, int p);
-	void takeTurn();
-	int gWon;
-	int sWon;
+    
+    int accuracy;
+    int score;
+    state curr_state;
+    int darts;
+    int bull(int p);
+    int throw_single(int single);
+    int throw_double(int d);
+    int throw_treble(int d, int p);
+    void takeTurn();
+    int gWon;
+    int sWon;
 };
 
 #endif
